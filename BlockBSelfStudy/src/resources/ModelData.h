7c18e4faee5fad1a8edcfad43ca27b054313a670
DylanoSol <dylanosolter@hotmail.com>
2022-12-02 11:00:11 +0100

    [ADDED] ModelData class from OcEngine

---
+++ BlockBSelfStudy/src/resources/ModelData.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+#include <tiny_gltf.h>
+#include <PhysX/PxPhysicsAPI.h>
+
+//Implementation derived from the OcEngine. 
+
+	struct MeshInstance
+	{
+		uint32_t m_meshDataIdx;
+
+		physx::PxVec3 m_position{};
+		physx::PxQuat m_rotation{};
+		physx::PxVec3 m_scale{ 1, 1, 1 };
+
+		std::string m_diffuseTexture;
+	};
+
+	struct MeshData
+	{
+		std::string m_meshName = "unnamed";
+		std::string uuid = "";
+
+		std::vector<physx::PxVec3> m_positions;
+		std::vector<physx::PxVec3> m_normals;
+		std::vector<physx::PxVec2> m_texCoords;
+
+		std::vector<uint32_t> m_indices;
+		tinygltf::Node node;
+	};
+
+	struct ModelData
+	{
+		~ModelData()
+		{
+			for (size_t i = 0; i < m_meshes.size(); i++)
+			{
+				delete m_meshes[i];
+			}
+		}
+
+		std::vector<MeshInstance*> m_instances;
+		std::vector<MeshData*> m_meshes;
+	};
