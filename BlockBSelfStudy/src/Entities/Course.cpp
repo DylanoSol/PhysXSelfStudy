11ae9e97e191d7540162cc09f5f3c63a5551cfe4
DylanoSol <dylanosolter@hotmail.com>
2022-12-09 15:51:52 +0100

    [FIXED] Cleaned up unnecessary comments and temporary test names

--- BlockBSelfStudy/src/Entities/Course.cpp
+++ BlockBSelfStudy/src/Entities/Course.cpp
@@ -18,9 +18,6 @@ Course::Course(physx::PxVec3(position), physx::PxQuat(rotation), physx::PxVec3(s
 
 
 	//Generate triangle mesh
-
-	std::vector<glm::vec3> test;
-
 	for (size_t i = 0; i < m_courseData->m_instances.size(); i++)
 	{
 		auto& instance = m_courseData->m_instances[i];
@@ -28,48 +25,14 @@ Course::Course(physx::PxVec3(position), physx::PxQuat(rotation), physx::PxVec3(s
 
 		for (size_t j = 0; j < meshData->m_indices.size(); j++)
 		{
-			test.push_back(meshData->m_positions[meshData->m_indices[j]]);
+			m_vertices.push_back(meshData->m_positions[meshData->m_indices[j]]);
 		}
 	}
-
 	
-
-	//for (size_t i = 0; i < m_indices.size(); i++)
-	//{
-	//	test.push_back(m_vertices[m_indices[i]]);
-	//}
-
-	////Get the vertices
-	//for (size_t i = 0; i < m_courseData->m_meshes.size(); i++)
-	//{
-	//	for (size_t j = 0; j < m_courseData->m_meshes[i]->m_positions.size(); j++)
-	//	{
-	//		m_vertices.push_back(m_courseData->m_meshes[i]->m_positions[j]);
-	//	}
-	//}
-
-	//int idxOffset = 0;
-
-	////Get the indices 
-	//for (size_t i = 0; i < m_courseData->m_meshes.size(); i++)
-	//{
-	//	
-
-	//	idxOffset += m_courseData->m_meshes[i]->m_indices.size();
-	//}
-
-	
-
 	physx::PxTriangleMeshDesc meshDescription; 
-	meshDescription.points.count = test.size(); 
+	meshDescription.points.count = m_vertices.size(); 
 	meshDescription.points.stride = sizeof(physx::PxVec3); 
-	meshDescription.points.data = &test[0]; 
-
-	//For indexed meshes
-	
-//	meshDescription.triangles.count = m_indices.size(); 
-//	meshDescription.triangles.stride = sizeof(physx::PxU32); 
-//	meshDescription.triangles.data = &m_indices[0]; 
+	meshDescription.points.data = &m_vertices[0]; 
 	
 	assert(meshDescription.isValid());
 
